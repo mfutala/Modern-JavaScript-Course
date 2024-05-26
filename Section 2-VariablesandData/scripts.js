b66ab4508ccfae6d2c62e6849ca2b983a5fa6213
mfutala <mfutala@hotmail.com>
2024-05-26 17:18:24 -0400

    Updating index and scripts file

---
+++ Section 2-VariablesandData/scripts.js
@@ -0,0 +1,19 @@
+console.log(300);
+console.log("Hellos");
+console.error('Danger');
+console.table({name: 'Brad', email: 'Brad@email.com'});
+
+
+
+//comment
+let firstName = 'John';
+
+
+
+
+
+
+
+
+
+
