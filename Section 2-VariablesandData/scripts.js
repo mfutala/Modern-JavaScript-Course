cbee6b63c243eef5535555ceb925b44fd0ed8cad
mfutala <mfutala@hotmail.com>
2024-06-13 14:46:11 -0400

    Update on 6/13/24

--- Section 2-VariablesandData/scripts.js
+++ Section 2-VariablesandData/scripts.js
@@ -1,16 +1,35 @@
-console.log(300);
+/* console.log(300);
 console.log("Hellos");
 console.error('Danger');
 console.table({name: 'Brad', email: 'Brad@email.com'});
-
+ */
 
 
 //comment
-let firstName = 'John';
+let firstName = '30';
+
+let aval; 
+
+aval = firstName;
+
+aval = +aval;
+
+aval = "Hello world, I am 30 years old!";
+
+let aWord = 'help';
+
+x = aval[1];
+
+//x = aval.__proto__;
+
+x = aWord.toLowerCase().replace(/(^| )(\w)/g, s => s.toUpperCase())
 
 
 
+aWord = aWord.charAt(0).toUpperCase() + aWord.substring(1, aWord.length);
 
+console.log(aWord);
+ 
 
 
 
